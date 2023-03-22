0fb4c6b2a9b78dca303241bc307e93ba82aefd06
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2023-03-22 18:54:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,30 @@ function setup(){
     canvas.mouseReleased(classifyCanvas);
     synth=window.speechSynthesis;
 }
-function preLoad(){
+function preload(){
     classifier=ml5.imageClassifier('DoodleNet');
 }
 function clearCanvas(){
     background("white");
+}
+function draw(){
+    strokeWeight(13);
+    stroke("black");
+    if (mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    console.log(results);
+    var result=results[0].label;
+    document.getElementById('label').innerHTML='Nome: '+result.replace('_',' ');
+    document.getElementById('confidence').innerHTML='Precisâo: '+Math.round(results[0].confidence*100) + '%';
+    utterThis= new SpeechSynthesisUtterance(result.replace('_', ' '));
+    synth.speak(utterThis);
 }
